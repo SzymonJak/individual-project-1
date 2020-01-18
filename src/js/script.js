7f00c23309e028716149cc6b4da4280eef0f7c4c
Szymon Jakubiak <jakubiak.sz@gmail.com>
2020-01-18 15:56:30 +0100

    Delete console.log

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,6 @@ const elementSidebar = document.querySelector('.sidebar-list');
 const elementWrapper = document.querySelector('.content-wrapper');
 
 buttonHamburger.addEventListener('click', function(){
-  console.log('here');
   elementSidebar.classList.toggle('show');
   elementWrapper.classList.toggle('wrapper-narrow');
 });
